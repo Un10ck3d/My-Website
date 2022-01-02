9c06b77a8b4b2bdcd91008b6437edd0dda42046e
Jonathan <jonathan@bangert.dk>
2022-01-02 20:02:56 +0100

    I works

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php
-phpinfo();
-?>
+<?php echo file_get_contents("html/header.html"); ?> 
+<?php echo file_get_contents("html/body.html"); ?> 
+<?php echo file_get_contents("html/footer.html"); ?> 
